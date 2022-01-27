42f32242765b00e79a85328efc65fe4157acb8d6
ahjamalm <ahjamalm@gmail.com>
2022-01-27 13:48:11 +0400

    Update MediaControls.style.ts

--- src/MediaControls.style.ts
+++ src/MediaControls.style.ts
@@ -22,7 +22,7 @@ export default StyleSheet.create({
     alignItems: "center",
     alignSelf: "stretch",
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "space-between",
   },
   fullScreenContainer: {
     alignItems: "center",
